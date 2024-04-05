c3f3131c8b003a4ced6125d75e0522ccfec6f849
silva3265 <silva3265@gmail.com>
2024-04-05 16:45:48 -0300

    Atualizando objetos com o método merge

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -10,11 +10,31 @@ import java.math.BigDecimal;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
+    public void atualizarObjeto() {
+        Produto produto = new Produto();
+
+        produto.setId(1);
+        produto.setNome("Kindle Paperwhite");
+        produto.setDescricao("Conheça o novo Kindle.");
+        produto.setPreco(new BigDecimal(599));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificacao);
+        Assert.assertEquals("Kindle Paperwhite", produtoVerificacao.getNome());
+    }
+
+    @Test
     public void removerObjeto() {
         Produto produto = entityManager.find(Produto.class, 3);
 
         entityManager.getTransaction().begin();
-        entityManager.remove(produto); // Recebe uma Entidade
+        entityManager.remove(produto);
         entityManager.getTransaction().commit();
 
 //        entityManager.clear(); Não é necessário na asserção para operação de remoção.
